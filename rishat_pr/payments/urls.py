7cdb6def9b5845d48fada6ec5eba5988377a91ce
Sargis <saqozzz1970@gmail.com>
2022-09-11 13:44:13 +0400

    Добавлен name в urls

--- rishat_pr/payments/urls.py
+++ rishat_pr/payments/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from .views import get_item, buy_item, buy_order, ok, fail
 
 urlpatterns = [
-    path('item/<int:pk>', get_item),
-    path('buy/<int:pk>', buy_item),
-    path('order', buy_order),
-    path('success', ok),
-    path('cancel', fail)
+    path('item/<int:pk>', get_item, name='detail'),
+    path('buy/<int:pk>', buy_item, name='buy'),
+    path('order', buy_order, name='order'),
+    path('success', ok, name='ok'),
+    path('cancel', fail, name='fail')
 ]
